06724280dba4c857cae25c96438e4de719f7c99d
tugolbay-subanbekov <subanbekovt@gmail.com>
2022-03-24 18:54:40 +0600

    Добавлены остальные операции

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from api_v1.views import add_view
+from api_v1.views import add_view, subtract_view, multiply_view, divide_view
 
 
 app_name = "api_v1"
 
 urlpatterns = [
     path('add/', add_view, name='add'),
-    # path('subtract/', subtract_view, name='subtract'),
-    # path('multiply/', multiply_view, name='multiply'),
-    # path('divide/', divide_view, name='divide'),
+    path('subtract/', subtract_view, name='subtract'),
+    path('multiply/', multiply_view, name='multiply'),
+    path('divide/', divide_view, name='divide'),
 
 ]
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,9 +1,7 @@
 import json
 from http import HTTPStatus
 
-from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.http import JsonResponse, HttpResponseNotAllowed
 
 
 def add_view(request):
@@ -16,7 +14,63 @@ def add_view(request):
             {
                 "value": value,
             },
+            status=HTTPStatus.OK
+        )
+
+    return HttpResponseNotAllowed(["GET", "POST"])
+
+
+def subtract_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        a = int(data['a'])
+        b = int(data['b'])
+        value = a - b
+        return JsonResponse(
+            {
+                "value": value,
+            },
             status=HTTPStatus.CREATED
         )
+
+    return HttpResponseNotAllowed(["GET", "POST"])
+
+
+def multiply_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        a = int(data['a'])
+        b = int(data['b'])
+        value = a * b
+        return JsonResponse(
+            {
+                "value": value,
+            },
+            status=HTTPStatus.CREATED
+        )
+
+    return HttpResponseNotAllowed(["GET", "POST"])
+
+
+def divide_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        a = int(data['a'])
+        b = int(data['b'])
+        try:
+            value = a / b
+            return JsonResponse(
+                {
+                    "value": value,
+                },
+                status=HTTPStatus.OK
+            )
+        except ZeroDivisionError as e:
+            return JsonResponse(
+                {
+                    "value": error,
+                },
+                status=HTTPStatus.BAD_REQUEST
+            )
 
     return HttpResponseNotAllowed(["GET", "POST"])
